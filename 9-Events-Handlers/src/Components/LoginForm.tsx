9a92488048cc42f7dec2c375f343c94ec2379433
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2025-05-16 01:47:06 +0500

    add taget,useState and store user data in variable which we can latter send to api

--- 9-Events-Handlers/src/Components/LoginForm.tsx
+++ 9-Events-Handlers/src/Components/LoginForm.tsx
@@ -1,14 +1,29 @@
-import type { SyntheticEvent } from "react"
-
+import {type SyntheticEvent, useState } from "react"
 const LoginForm=()=>{
+    const[email , setEmail]= useState('')
+    const[password , setPassword]= useState('')
     const submitForm = (ev: SyntheticEvent)=>{
         ev.preventDefault();
+        const target = ev.target as HTMLFormElement;
+        //  You're telling TypeScript that ev.target is a form.
+        //  This helps with type safety and accessing form elements.
+
+
+        console.log(target,{
+            email,
+            password
+        });
+        
     }
     return(
         <>
         <form className="flex flex-col gap-4" onSubmit={submitForm}>
-            <input type="email" placeholder="Enter Your Email"/>
-            <input type="password" placeholder="Enter Your Password" />
+            <input onChange={(ev)=>{
+                setEmail(ev.target.value);
+            }} type="email" placeholder="Enter Your Email"/>
+            <input onChange={(ev)=>{
+                setPassword(ev.target.value);
+            }} type="password" placeholder="Enter Your Password" />
             <button>Submit</button>
         </form>
         </>
